df05867a5971ed3fe72b50cd14293ba0a037e405
rue-oplo <rueben.cumberbatch@myoplo.co.uk>
2022-11-23 15:50:28 +0000

    render service users in table

--- pages/service-users/index.tsx
+++ pages/service-users/index.tsx
@@ -1,14 +1,80 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Heading,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 import { NextPage } from "next";
 import React from "react";
 import Layout from "../../components/Layout";
 
-const ServiceUsers: NextPage = () => {
+type ServiceUser = {
+  id: number;
+  uid: string;
+  name: string;
+  surname: string;
+  dateOfBirth: Date | null;
+  createdAt: Date;
+  movedIn: Date | null;
+  updatedAt: Date;
+  conditions: string[];
+};
+
+type PageProps = {
+  serviceUsers: ServiceUser[];
+};
+
+const ServiceUsers: NextPage<PageProps> = ({ serviceUsers }) => {
+  console.log(serviceUsers[0]);
+
   return (
     <Layout>
-      <Heading>Service Users</Heading>
+      <Flex flexDirection="column">
+        <Heading>Service Users</Heading>
+        <TableContainer overflowY="auto">
+          <Table variant="striped" colorScheme="teal" size="sm">
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Name</Th>
+                <Th>Surname</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {serviceUsers?.map(
+                (serviceUser: ServiceUser, serviceUserIndex: number) => (
+                  <Tr key={serviceUserIndex}>
+                    <Td>{serviceUser.id}</Td>
+                    <Td>{serviceUser.name}</Td>
+                    <Td>{serviceUser.surname}</Td>
+                  </Tr>
+                )
+              )}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      </Flex>
     </Layout>
   );
 };
 
 export default ServiceUsers;
+
+export async function getServerSideProps() {
+  const response = await fetch(
+    `${process.env.API_ENDPOINT as string}/service-user`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      serviceUsers: data,
+    },
+  };
+}
